205e37fa8402278bae3160d4cf98f785a3c4e381
Ron Waldon <ron@blinkmobile.com.au>
2012-01-19 23:44:56 +0000

    partial reintegration merge from "shane/blinkgap-1_2_0-merge-b" back into "bf2js"

--- public_html/_R_/common/3/source/BlinkDispatch.js
+++ public_html/_R_/common/3/source/BlinkDispatch.js
@@ -3,30 +3,55 @@
  * requires utilities.js
  */
 
+/*jslint white:true*/
+
 (function(window, undefined) {
+	'use strict';
+	var $ = window.jQuery,
 	BlinkDispatch = function(interval) {
-		var queue = [],
-			timeout = null,
-			dispatch = this; // to facilitate self-references
-		this.interval = interval;
+		var timeout = null,
+		self = this; // to facilitate self-references
+		/* END: var */
+		this.queue = [];
+		this.interval = interval || 0;
 		this.isPaused = false;
+		
+		/**
+		 * check to see if we have items in the queue, and process the first one
+		 */
 		function processQueue() {
-			if (dispatch.isPaused || timeout !== null || queue.length === 0) {return;}
-			var item = queue.shift();
+			if (self.isPaused || timeout !== null || self.queue.length === 0) {return;}
+			var item = self.queue.shift(),
+			result;
+			/* END: var */
 			if (typeof item === 'function') {
-				item();
-			} else {
-				log('BlinkDispatch:' + item);
+				result = item();
 			}
-			timeout = setTimeout(function() {
-				timeout = null;
-				processQueue();
-			}, dispatch.interval);
+			// check to see if we have queued a jQuery Deferred Promise
+			if (typeof $ === 'function' && $() instanceof $
+					&& result && typeof result === 'object'
+					&& result.promise && typeof result.promise === 'function') {
+				timeout = 0; // placeholder value
+				$.when(result)
+				.always(function() {
+					timeout = setTimeout(function() {
+						timeout = null;
+						processQueue();
+					}, self.interval);
+				});
+			} else { // not a Promise
+				timeout = setTimeout(function() {
+					timeout = null;
+					processQueue();
+				}, self.interval);
+			}
 		}
-		this._push = function(item) {
-			queue.push(item);
+		/**
+		 * public function to trigger processing of the queue
+		 */
+		this.poke = function() {
 			processQueue();
-		};
+		}
 		this.pause = function() {
 			clearTimeout(timeout);
 			timeout = null;
@@ -38,7 +63,11 @@
 		};
 		return this;
 	};
+	/* END: var */
 	BlinkDispatch.prototype.add = function(item) {
-		this._push(item);
+		this.queue.push(item);
+		this.poke();
 	};
+	
+	window.BlinkDispatch = BlinkDispatch;
 }(this));
