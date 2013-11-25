d278bb30611c8b07af758fb4412d2d4da77ca884
Ron Waldon <ron@blinkmobile.com.au>
2013-11-25 15:23:14 +1100

    BICv2: move JavaScript files

--- public_html/_R_/BICv2/js/lib/BMP.BIC.Config.js
+++ public_html/_R_/BICv2/js/lib/BMP.BIC.Config.js
@@ -7,7 +7,7 @@
   'use strict';
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['underscore'], function(_) {
+    define(['../../../../.'], function(_) {
       // Also create a global in case some scripts
       // that are loaded still are looking for
       // a global even when an AMD loader is in use.
